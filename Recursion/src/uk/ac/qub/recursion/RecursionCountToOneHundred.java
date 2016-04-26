2e2b6290dcf832377cd44d7ef7d764e1ecc5a10f
peter190190 <pcarson190190@gmail.com>
2016-04-26 22:03:52 +0100

    Comments added for clarity.

---
+++ Recursion/src/uk/ac/qub/recursion/RecursionCountToOneHundred.java
@@ -0,0 +1,56 @@
+package uk.ac.qub.recursion;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class RecursionCountToOneHundred {
+
+	private static Scanner scanner = new Scanner(System.in);
+	private static int number = 0;
+	
+	public RecursionCountToOneHundred() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome");
+		checkRange();
+		System.out.println("You entered : " + number);
+		countUp(number);
+
+	}
+
+	public static void countUp(int num) {
+		
+		if(num == 101) {
+			// Base case
+			System.out.println();
+		} else {
+			System.out.println(num);
+			countUp(num + 1);
+		}
+	}
+	
+	public static void checkRange() {
+		
+		System.out.println("Please enter a number between 1-10 inclusive");
+		
+		try {
+			number = scanner.nextInt();
+		} catch (InputMismatchException ex) {
+			System.out.println("Invalid input, ensure you are using an integer.");
+			ex.printStackTrace();
+			scanner.next();
+			System.out.println();
+		}
+		
+		// Check base case - i.e number is between 1-10
+		if(number > 0 && number < 11) {
+		// Base case
+			System.out.println("Number OK");
+		} else {
+			checkRange();
+		}
+	}
+}
